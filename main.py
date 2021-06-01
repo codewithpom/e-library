1e42643b927acb8f657158195b6fb572bbb5a190
PadmashreeJha <>
2021-06-01 04:58:13 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,62 @@
+import json
+import os
+from flask import Flask, render_template, request
+import requests
+from flask_cors import CORS
+app = Flask('app')
+base_url = "https://raw.githubusercontent.com/MrAlex6204/Books/master/"
+
+CORS(app)
+
+
+def remove_suffix(input_string, suffix):
+    if suffix and input_string.endswith(suffix):
+        return input_string[:-len(suffix)]
+    return input_string
+
+
+@app.route("/")
+def hello():
+    return render_template('index.html')
+
+
+@app.route("/books")
+def return_books():
+    response = requests.get("https://api.github.com/repos/MrAlex6204/Books/git/trees/master?recursive=1")
+    data_in_json = response.json()
+    finished_data = {"data": []}
+    for data in data_in_json['tree']:
+        if str(data['path']).endswith(".pdf"):
+            finished_data['data'].append(remove_suffix(str(data['path']), ".pdf"))
+
+    print(json.dumps(finished_data))
+    return json.dumps(finished_data)
+
+
+@app.route("/read", methods=['POST'])
+def read_book():
+    if 'book' in dict(request.form).keys():
+
+        book_name = request.form['book']
+
+        print(book_name)
+
+        complete_url = base_url + book_name
+
+        file = requests.get(complete_url)
+        if not os.path.exists(os.path.join('static/books', book_name)):
+                create = open(os.path.join('static/books', book_name), 'x')
+                create.close()
+                file_edited = open(os.path.join('static/books', book_name), 'wb')
+
+                file_edited.write(file.content)
+
+                file_edited.close()
+
+        return render_template("read.html", book=book_name, source=os.path.join('static/books', book_name))
+
+    else:
+        return "Sorry content not available"
+
+
+app.run(host="0.0.0.0", port=80808, debug=True)
---
+++ static/script.js
@@ -0,0 +1,84 @@
+// noinspection JSUnfilteredForInLoop,JSDeprecatedSymbols
+
+const base_url = "https://github.com/MrAlex6204/Books/raw/master/"
+function show_books() {
+    const list_of_books = document.getElementById("list_of_books")
+    $.get("/books", function (data){
+        const json_data = JSON.parse(data)['data']
+        for (let jsonDataKey in json_data) {
+
+            console.log(json_data[jsonDataKey])
+
+            let book_name_tag = document.createElement('li')
+
+            book_name_tag.innerHTML = json_data[jsonDataKey].toString()
+
+            book_name_tag.setAttribute("class", "list-group-item")
+
+            let anchor_tag_download = document.createElement("a")
+
+            let download_button = document.createElement("button")
+
+            let anchor_tag_read = document.createElement("a")
+
+            let read_button = document.createElement("button")
+
+            read_button.innerHTML  = "Read it"
+
+            read_button.setAttribute("class", 'btn-primary')
+            read_button.setAttribute("style", 'float: right; margin-right: 20px')
+            read_button.setAttribute("name", 'read')
+
+            anchor_tag_read.append(read_button)
+
+            download_button.setAttribute("class", 'btn-primary')
+
+            anchor_tag_download.setAttribute("href", base_url + json_data[jsonDataKey] + '.pdf')
+
+            download_button.setAttribute("style", "float: right")
+
+            download_button.innerHTML = "Download it"
+
+            anchor_tag_download.append(download_button)
+
+            book_name_tag.append(anchor_tag_download)
+            book_name_tag.append(anchor_tag_read)
+            book_name_tag.style.wordWrap = "break-word"
+
+            list_of_books.append(book_name_tag)
+
+
+        }
+
+    });
+
+
+
+}
+
+
+// noinspection JSDeprecatedSymbols
+document.getElementById("list_of_books").addEventListener("click", function(){
+    console.clear()
+    console.log("click")
+    console.log(event.target.tagName)
+    console.log(event.target.name)
+    // noinspection JSDeprecatedSymbols
+    if (event.target.tagName === "BUTTON" && event.target.name === "read"){
+        const full_file_name = event.target.parentNode.parentNode.innerText.split("\n")[0] + '.pdf'
+        console.log(full_file_name)
+        document.getElementsByName('book')[0].value = full_file_name
+        document.getElementById('submit').click()
+
+
+    }
+
+});
+
+
+
+
+
+
+
+
